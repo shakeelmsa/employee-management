98e0aa2d7515feae07ea4df43c1d0ace1a088bff
Shakeel Ahmed <ahmed.shakeels@outlook.com>
2026-05-19 08:16:56 +0530

    Initial project upload Front End and Back End

---
+++ ems-frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className="footer">
+             <span>All rights reserved 2026 by Shakeel Ahmed</span>
+        </footer>
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ ems-frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+        <header>
+            <nav>
+                <a href="https://github.com/shakeelmsa">Employee Management System</a>
+            </nav>
+        </header>
+      
+    </div>
+  )
+}
+
+export default Header
